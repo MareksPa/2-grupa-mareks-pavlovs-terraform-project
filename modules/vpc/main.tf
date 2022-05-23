1b63c86963743301b1c3f4d4bba11683a412f442
Mareks <pavlovsmareks@gmail.com>
2022-05-23 10:48:47 +0300

    Changes to main.tf file

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,3 +11,47 @@ resource "aws_vpc" "main" {
     Name = "VPC Module"
   }
 }
+
+resource "aws_subnet" "public" {
+  # count    = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+ # count    = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Main IGW"
+  }
+}
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "NAT Gateway EIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "Main NAT Gateway"
+  }
+}
